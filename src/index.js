da69d359e755f8550aa379289eac571b2b2008d8
Amit Prakash <amitprakash0111@gmail.com>
2021-07-22 16:31:00 +0530

    initial commit for react essential course

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+ReactDOM.render(
+  React.createElement("h1",{style : {color : "blue"}}, "hey everyone"),
+  document.getElementById('root')
+);
+
+
