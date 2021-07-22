c2774c4876280fd057a4bde9d98b025c9bc5d2a2
Amit Prakash <amitprakash0111@gmail.com>
2021-07-22 16:46:24 +0530

    add a simple component named App

--- src/index.js
+++ src/index.js
@@ -4,11 +4,7 @@ import './index.css';
 import App from './App';
 
 ReactDOM.render(
-  <ul>
-    <li>Monday</li>
-    <li>Tuesday</li>
-    <li>Wednesday</li>
-  </ul>,
+  <App />,
   document.getElementById('root')
 );
 
