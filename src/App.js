92406334362a6960e637b43a9b4100b7cbe490fb
Amit Prakash <amitprakash0111@gmail.com>
2021-07-22 16:58:05 +0530

    adding multiple components inside a div

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
 
+function Header() {
+  return (
+    <header>
+    <h1>Eve's Kitchen</h1>
+    </header>
+  );
+}
+
+function Main() {
+  return (
+    <section>
+    <p>We serve the most delicious food around.</p>
+    </section>
+  );
+}
+
+function Footer() {
+  return (
+    <footer>
+    <p>It's true.</p>
+    </footer>
+  );
+}
+
 //a component using method
 function App() {
   return (
     <div className="App">
-      <h1>Header</h1>
-      <h2>Main</h2>
-      <h3>Footer</h3>
+      <Header />
+      <Main />
+      <Footer />
     </div>
   );
 }
