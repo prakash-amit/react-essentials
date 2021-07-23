eb5cbb478b3ab6459080a0afd88aa2bb181cd238
prakash-amit <amit.prakash.se@gmail.com>
2021-07-23 11:30:02 +0530

    configure router and incorporating link component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import React, {useState, useEffect} from 'react';
+import React from 'react';
 import './App.css';
+import {Routes, Route} from "react-router-dom";
+import {Home, About, Events, Contact, Whoops404} from "./pages";
 
 //a component using method
 function App() {
 
     return (
-      <>
-      <h1>hi react testing library</h1>
-      </>
+      <div>
+      <Routes>
+        <Route  path = "/" element = {<Home />}></Route>
+        <Route path = "/About" element = {<About />}></Route>
+        <Route path = "/Events" element = {<Events />}></Route>
+        <Route path = "/Contact" element = {<Contact />}></Route>
+        <Route path = "*" element = {<Whoops404 />}></Route>
+      </Routes>
+      </div>
     );
 }
 
